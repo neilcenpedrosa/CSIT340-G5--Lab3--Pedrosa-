b283e6e3ec5a33f5a272c7c1e776e9fa90b1ccce
Neilcen <cen.pedrosa08@gmail.com>
2026-09-21 18:27:52 +0800

    1.2: course information step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,34 @@
-const Header = ({ course }) => {
-  return (
-    <h1>{course}</h1>
-  )
-}
-
-const Part = ({ name, units }) => {
-  return (
-    <p>
-      {name} {units} units
-    </p>
-  )
-}
-
-const Content = ({ part1, part2, part3 }) => {
-  return (
-    <div>
-      <Part name={part1.name} units={part1.units} />
-      <Part name={part2.name} units={part2.units} />
-      <Part name={part3.name} units={part3.units} />
-    </div>
-  )
-}
-
-const Total = ({ part1, part2, part3 }) => {
-  return (
-    <p>
-      Number of units {part1.units + part2.units + part3.units}
-    </p>
-  )
-}
-
 const App = () => {
-  const course = 'YOUR CIT-U COURSE'
+  const course = 'Bachelor of Science in Information Technology'
 
-  const part1 = {
-    name: 'Data Analytics',
-    units: 3
-  }
+  const part1 = 'Data Analytics'
+  const exercises1 = 3
 
-  const part2 = {
-    name: 'Project Management for IT',
-    units: 3
-  }
+  const part2 = 'Project Management for IT'
+  const exercises2 = 3
 
-  const part3 = {
-    name: 'Information Management 2',
-    units: 3
-  }
+  const part3 = 'Information Management 2'
+  const exercises3 = 3
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
-      <Total
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
+      <h1>{course}</h1>
+
+      <p>
+        {part1} {exercises1} units
+      </p>
+
+      <p>
+        {part2} {exercises2} units
+      </p>
+
+      <p>
+        {part3} {exercises3} units
+      </p>
+
+      <p>
+        Number of units: {exercises1 + exercises2 + exercises3}
+      </p>
     </div>
   )
 }
